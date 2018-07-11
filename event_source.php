cf9d8d40d33803507e82f2d1eacf35cbd18aba94
Linus Rachlis <linus@rachlis.net>
2018-07-10 20:44:52 -0400

    Add ical output mode using eluceo/ical library

--- event_source.php
+++ event_source.php
@@ -2,6 +2,7 @@
 
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
+use Eluceo\iCal;
 
 require 'vendor/autoload.php';
 date_default_timezone_set('America/Toronto');
@@ -16,6 +17,9 @@ $crawler = $client->request('GET', $_GET['play_url']);
 $title = trim($crawler->filter('.page-title')->text());
 $runtime_text = $crawler->filter('.show-info')->first()->filter('.column.right dd')->text();
 $runtime_minutes = preg_replace('/^(\d+)m$/', '$1', $runtime_text);
+$location_name = $crawler->filter('.show-info > h3')->text();
+$location_address_html = $crawler->filter('.show-info > address')->html();
+$location_address = preg_replace('@<br( /)?>@', ', ', $location_address_html);
 
 $events = [];
 $crawler->filter('.performances table tbody tr')->each(
@@ -36,5 +40,75 @@ $crawler->filter('.performances table tbody tr')->each(
     }
 );
 
-header('Content-type: application/json');
-echo json_encode($events);
+if (!isset($_GET['format'])) $_GET['format'] = 'fullcalendar';
+
+switch ($_GET['format'])
+{
+    case 'ical':
+        output_ical($events);
+        break;
+    case 'fullcalendar':
+    default:
+        header('Content-type: application/json');
+        echo json_encode($events);
+        break;
+}
+
+// ----------------------------------
+
+function output_ical(array $events)
+{
+    global $location_name;
+    global $location_address;
+
+    header('Content-type: text/plain; charset=utf8');
+
+    $vCalendar = new iCal\Component\Calendar('fringr.linus.rachlis.net');
+
+    $torontoTz = new DateTimeZone('America/Toronto');
+    $utc = new DateTimeZone('UTC');
+
+    foreach ($events as $event)
+    {
+        $dtStart = new DateTime($event['start'], $torontoTz);
+        $dtStart->setTimezone($utc);
+        $dtEnd = new DateTime($event['end'], $torontoTz);
+        $dtEnd->setTimezone($utc);
+
+        $uid = sha1($event['title']) . '_' . strtotime($event['start']) . '@fringr.linus.rachlis.net';
+
+        $vEvent = new iCal\Component\Event();
+        $vEvent
+            ->setSummary($event['title'])
+            ->setDtStart($dtStart)
+            ->setDtEnd($dtEnd)
+            ->setLocation("$location_name, $location_address")
+            ->setUniqueId($uid)
+            ;
+
+        $vCalendar->addComponent($vEvent);
+
+        // echo "BEGIN:VEVENT\n";
+        // $title_safe = str_replace(["\n", "\r"], ' ', $event['title']);
+        // echo "SUMMARY:$title_safe\n";
+        // $uid = sha1($event['title']) . '_' . strtotime($event['start']) . '@fringr.linus.rachlis.net';
+        // echo "UID:$uid\n";
+        // echo "DTSTART:$event[start]\n";
+        // echo "DTEND:$event[end]\n";
+        // $location_safe = str_replace(["\n", "\r"], ' ', "$location_name, $location_address");
+        // echo "LOCATION:$location_safe\n";
+        // echo "END:VEVENT\n";
+        // echo "\n";
+        // echo "\n";
+        // echo "\n";
+        // echo "\n";
+        // echo "\n";
+        // echo "\n";
+        // echo "\n";
+        // echo "\n";
+    }
+
+    // echo "END:VCALENDAR\n";
+
+    echo $vCalendar->render();
+}
